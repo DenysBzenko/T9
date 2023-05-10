427d74a9ff194551b17b0c728b7f0918676dbb5d
Denys Bzenko <dbzenko@kse.org.ua>
2023-05-11 00:05:55 +0300

    Algoritm

--- T9A/T9A/Program.cs
+++ T9A/T9A/Program.cs
@@ -4,10 +4,10 @@ using System.IO;
 
 class Program
 {
-    static void Main()
+    private static void Main()
     {
         // Зчитуємо вміст файлу зі словами
-        string[] words = File.ReadAllLines("/Users/zakerden1234/Desktop/T9/T9A/T9A/words_list.txt");
+        string[] words = File.ReadAllLines("C:\\Users\\User\\T9\\T9A\\T9A\\words_list.txt");
 
         // Перетворюємо масив слів у список для зручності перевірки
         List<string> wordList = new List<string>(words);
@@ -25,6 +25,9 @@ class Program
         {
             if (!wordList.Contains(word.ToLower()))
             {
+                List<string> fiveOfferedWords = GetFiveOfferedWords(word.ToLower(), wordList);
+                Console.Write($"Did you mean '{word}'? Try these words instead: ");
+                Console.WriteLine(string.Join(", ", fiveOfferedWords));
                 unknownWords.Add(word);
             }
         }
@@ -32,12 +35,84 @@ class Program
         // Виводимо підказку, якщо знайдено невідомі слова
         if (unknownWords.Count > 0)
         {
-            Console.Write("Looks like you have typos in next words: ");
-            Console.WriteLine(string.Join(", ", unknownWords));
+            Console.WriteLine($"Looks like you have typos in {unknownWords.Count} words.");
         }
         else
         {
             Console.WriteLine("All words are correct!");
         }
+    }
+    private static List<string> GetFiveOfferedWords(string wrongWord, List<string> glossary)
+    {
+        Dictionary<string, int> wordsAndValues = new Dictionary<string, int>();
+        foreach (var correctWord in glossary)
+        {
+            bool needToBeAdded = DoesWordNeedToBeAdded(wordsAndValues, wrongWord, correctWord);
+            if (needToBeAdded)
+            {
+                wordsAndValues[correctWord] = FindLCS(wrongWord, correctWord);
+            }
+        }
+
+        // Сортуємо слова за значенням і повертаємо перші п'ять
+        var sortedWords = wordsAndValues.OrderBy(x => x.Value).Select(x => x.Key).ToList();
+        return sortedWords.Take(5).ToList();
+    }
+
+
+    private static bool DoesWordNeedToBeAdded(Dictionary<string, int> wordsAndValues, string wrongWord,
+        string correctWord)
+    {
+        string minValueOfKey = null;
+        int lcsOfTheOfferedWord = FindLCS(wrongWord, correctWord);
+        if (wordsAndValues.Keys.Count < 5)
+        {
+            return true;
+        }
+
+        foreach (var key in wordsAndValues.Keys)
+        {
+            if (minValueOfKey == null || wordsAndValues[key] < wordsAndValues[minValueOfKey])
+            {
+                minValueOfKey = key;
+            }
+        }
+
+        if (minValueOfKey != null && wordsAndValues[minValueOfKey] < lcsOfTheOfferedWord)
+        {
+            wordsAndValues.Remove(minValueOfKey);
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    public static int FindLCS(string misstipedWord, string offeredWord)
+    {
+
+        int mistypedLen = misstipedWord.Length;
+        int offeredLen = offeredWord.Length;
+        int[,] tableLCS = new int[mistypedLen + 1, offeredLen + 1];
+
+
+
+        for (int m = 1; m <= mistypedLen; m++)
+        {
+            for (int o = 1; o <= offeredLen; o++)
+            {
+                if (misstipedWord[m - 1] == offeredWord[o - 1])
+                {
+                    tableLCS[m, o] = tableLCS[m - 1, o - 1] + 1;
+                }
+                else
+                {
+                    tableLCS[m, o] = Math.Max(tableLCS[m - 1, o], tableLCS[m, o - 1]);
+                }
+            }
+        }
+
+        return tableLCS[mistypedLen, offeredLen];
     }
 }
